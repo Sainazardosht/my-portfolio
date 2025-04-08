660eeecf7b776bb86a1823dfec96e3f2fcf6374a
Sainazardosht <sainazardosht@gmail.com>
2025-04-08 16:23:33 -0700

    update the content in the project section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,6 +10,8 @@ function Card(props) {
         <div className='m-5 py-1 border-slate-800 border-2 rounded-lg overflow-hidden bg-slate-800 text-stone-100' data-aos='zoom-out'>
         <img src={props.imgURL} alt="project images" className='border-b-1'/>
         <h4 className='text-xl font-semibold py-3'>{props.name}</h4>
+        <p className='text-left px-5 py-3'>{props.about}</p>
+        <h4 className='text-l font-semibold py-3'>Technologies: {props.tech}</h4>
         <a href={props.URL} target="_blank" className='text-md p-2 text-orange-500 hover:bg-orange-500 hover:text-stone-100 transition ease-out duration-500 rounded-md '>Explore More in GitHub</a>
         </div>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,29 +11,38 @@ function Projects() {
     <section className="sm:p-25 p-10 text-center bg-stone-100 text-slate-800">
       <h2 className="text-4xl font-semibold pb-20 font-serif">Projects</h2>
       <div className="md:grid grid-cols-3 gap-10">
-        <Card
-          imgURL={rosee}
-          name="Rosee Project"
-          URL="https://github.com/Sainazardosht"
-        />
+        
         <Card
           imgURL={reen}
           name="Reen Project"
+          about="A resposive website built with clean HTML and CSS, designed with a modern layout for showcasing content effectively."
+          tech="HTML, CSS, Bootstrap"
           URL="https://github.com/Sainazardosht"
         />
         <Card
           imgURL={dentalClinic}
           name="Dental Clinic Project"
+          about="A professional website for a dental clinic, featuring smooth animations, responsive design, and a contact form."
+          tech="HTML, CSS, JavaScript, Bootstrap"
           URL="https://github.com/Sainazardosht"
         />
         <Card
           imgURL={large}
           name="Large Project"
+          about="A mini web project focused on layout and styling practice, showcasing responsive design and clean structure."
+          tech="HTML, CSS, JavaScript"
+          URL="https://github.com/Sainazardosht"
+        />
+        <Card
+          imgURL={rosee}
+          name="Rosee Project"
+          tech="HTML, CSS, React, Javascript"
           URL="https://github.com/Sainazardosht"
         />
         <Card
           imgURL={tinDog}
           name="Tin Dog Project"
+          tech="HTML, CSS, BootStrap"
           URL="https://github.com/Sainazardosht"
         />
       </div>
